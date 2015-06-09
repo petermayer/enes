076cf5d966245fd7847181839f310c6c3b298abc
Peter Mayer <peter.mayer@secuso.org>
2015-06-09 18:48:36 +0200

    License change to GPLv3

--- src/estimators/click/entropy/ClickEntropyEstimatorIndep.java
+++ src/estimators/click/entropy/ClickEntropyEstimatorIndep.java
@@ -1,18 +1,20 @@
 /*=========================================================================
- * Copyright 2013 EnEs Authors
+ * EnEs is a little tool for calculating multiple different metrics to
+ * analyze the distribution of password sets.
+ * Copyright (C) 2013 Peter Mayer
  *
- * Licensed under the Attribution-ShareAlike 3.0 Unported license (the 
- * "License"). You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * https://creativecommons.org/licenses/by-sa/3.0/legalcode
- * 
- * Or have a look at the tldr; version: 
- * 
- * https://creativecommons.org/licenses/by-sa/3.0/deed.en
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *=========================================================================*/
 package estimators.click.entropy;
 
